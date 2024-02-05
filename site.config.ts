433cbb897251ab84836f32aed3efcc394ffd4b52
squaaaaash <117624325+squaaaaash@users.noreply.github.com>
2024-02-05 16:33:02 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e6103f1419e243b8a6d1b41e1e15f551',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://www.notion.so/MayUBlue-e6103f1419e243b8a6d1b41e1e15f551?pvs=4',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
